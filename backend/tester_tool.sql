ce734c038b1f533d9fd4ebf30fe462187ba6c346
Julien Anquetil <anquetil.jul@gmail.com>
2020-01-29 23:11:00 +0100

    added database.sql

--- backend/tester_tool.sql
+++ backend/tester_tool.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 26 jan. 2020 à 22:24
+-- Généré le :  mer. 29 jan. 2020 à 22:10
 -- Version du serveur :  5.7.24
 -- Version de PHP :  7.2.14
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `login` (
   `is_admin` int(11) NOT NULL DEFAULT '0',
   `banned` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `login`
@@ -59,13 +59,14 @@ INSERT INTO `login` (`id`, `username`, `password`, `rank_id`, `is_admin`, `banne
 (16, 'Elwans', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 30, 0, 0),
 (17, 'Cames', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 50, 1, 0),
 (18, 'Walrosskastanie', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 50, 1, 0),
-(19, 'Gorem', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 10, 0, 0),
+(26, 'Gorem', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 10, 0, 0),
 (20, 'Dopxmine', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 10, 0, 0),
 (21, 'WhiteBaron', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 10, 0, 0),
 (22, 'Firekeeper', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 10, 0, 0),
 (23, 'Maisa', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 10, 0, 0),
 (24, 'Gouda', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 10, 0, 0),
-(25, 'IceClover', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 10, 0, 0);
+(25, 'IceClover', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 10, 0, 0),
+(19, 'Nikola', '07313f0e320f22cbfa35cfc220508eb3ff457c7e', 50, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -78,19 +79,23 @@ CREATE TABLE IF NOT EXISTS `post` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `message` varchar(1000) NOT NULL,
+  `to_x` varchar(30) NOT NULL DEFAULT 'TO BE TESTED',
   `user_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `post`
 --
 
-INSERT INTO `post` (`id`, `title`, `message`, `user_id`, `created_at`) VALUES
-(1, 'test', 'testest', 1, '2020-01-26 13:06:06'),
-(2, 'kyurem quest ', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit pharetra lobortis. In vel iaculis nulla, ac euismod est. Donec ullamcorper, libero quis placerat pellentesque, ligula tellus dignissim mi, id laoreet ipsum est et odio. Nunc fringilla risus euismod gravida bibendum. Suspendisse quis urna luctus, tristique mi et, efficitur nisi. Pellentesque eu mi eget metus elementum bibendum. Integer consequat blandit iaculis. Vivamus et justo et justo accumsan congue. Fusce fringilla commodo consequat. Proin imperdiet sodales lacinia. Nunc vel velit et felis aliquam semper. Praesent at molestie velit. Nulla venenatis leo sit amet viverra commodo. Phasellus a nisl volutpat nisi volutpat imperdiet. Fusce turpis ligula, pharetra vel ex id, pharetra efficitur augue. Integer sodales blandit maximus.', 1, '2020-01-26 13:06:06'),
-(3, 'zizi', 'qdfqdfvq sdfvqd qdfqsdf qdf qsdf qsd fqsdf \nzizi', 1, '2020-01-26 15:35:04');
+INSERT INTO `post` (`id`, `title`, `message`, `to_x`, `user_id`, `created_at`) VALUES
+(1, 'zizi', 'qdfqdfvq sdfvqd qdfqsdf qdf qsdf qsd fqsdf\r\nzizizizi', 'TO BE TESTED', 1, '2020-01-26 13:06:06'),
+(2, 'zizi', 'qdfqdfvq sdfvqd qdfqsdf qdf qsdf qsd fqsdf\r\nzizizizi', 'TO BE TESTED', 1, '2020-01-26 13:06:06'),
+(3, 'zizi', 'qdfqdfvq sdfvqd qdfqsdf qdf qsdf qsd fqsdf\r\nzizizizi', 'TO BE TESTED', 1, '2020-01-26 15:35:04'),
+(4, 'zizi', 'qdfqdfvq sdfvqd qdfqsdf qdf qsdf qsd fqsdf\r\nzizizizi', 'TO BE TESTED', 1, '2020-01-28 21:52:52'),
+(5, 'zizi', 'qdfqdfvq sdfvqd qdfqsdf qdf qsdf qsd fqsdf\r\nzizizizi', 'TO BE TESTED', 1, '2020-01-28 21:54:21'),
+(6, 'me too', 'I don\'t like you', 'TO BE TESTED', 1, '2020-01-29 19:59:47');
 
 -- --------------------------------------------------------
 
